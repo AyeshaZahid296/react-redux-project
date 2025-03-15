ca0d774e0d2fa93a55b736927fb69978c495d357
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-15 20:57:28 +0500

    store.js update

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import { createStore } from "redux";
 import reducers from "./reducers/index";
 
 const store = createStore(reducers, {});
-// this is store
+// this is store.js
 
 export default store;

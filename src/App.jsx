1a16b25de1d8ec969c5cf0c8815b27a7e47857ba
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-17 16:29:38 +0500

    BrowserRouter and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './container/Header';
+import ProductListing from './container/ProductListing';
+import ProductDetail from './container/ProductDetail';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<h2>404 Not Found!</h2>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;

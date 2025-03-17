275a77d11d4385b4a408f15104970f16c99fd0fe
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-18 00:38:18 +0500

    fetchapi added

--- src/container/ProductDetail.jsx
+++ src/container/ProductDetail.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { selectedProducts } from '../redux/actions/productActions';
+import { useSelector } from 'react-redux';
+
 
 const ProductDetail = () => {
+    const product = useSelector((state) => state.product);
+    const { productId } = useParams();
+    const dispatch = useDispatch();
+    console.log(productId);
+
+    const fetchProductDetail = () => {
+        const response = await axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch((err) => {
+                console.log(("Err", err));
+
+            })
+        dispatch(selectedProducts(response.data))
+    }
     return (
         <div>
             <h1>

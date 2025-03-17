e2c56a3c699fe8b8851d6647ac57a6ea0605ad72
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-17 15:54:58 +0500

    main.js updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore } from "redux";
 import reducers from "./reducers/index";
 
-const store = createStore(reducers, {});
-// this is store.js
+const store = createStore(
+    reducers,
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;

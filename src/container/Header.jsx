d7d8f38a7349e780dbc7060a84321ec4a4f11d6c
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-03-17 16:50:50 +0500

    ProductCopmonent updated

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='ui fixed menu'>
             <div className='ui container center'>
-                <h2>FakeShopes</h2>
+                <h2>FakeShope</h2>
             </div>
         </div>
     )
